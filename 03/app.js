bcfcac308cab9b67b0c88fba0989790e2e1e62e3
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-19 22:54:52 +0200

    adde some fix in NaN part of the excercise

--- 03/app.js
+++ 03/app.js
@@ -1,8 +1,8 @@
 export default function randomNumber(min, max) {
-    if(typeof min !== 'number')
+    if(typeof min !== 'number' && isNaN(min))
         throw new Error('Min arg is not a number')
     
-    if(typeof max !== 'number')
+    if(typeof max !== 'number' && isNaN(max))
         throw new Error('Max arg is not a number')
 
 
