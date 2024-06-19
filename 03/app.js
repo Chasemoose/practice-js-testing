c6892db7292e191a84b198735d5675da612925a1
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-19 22:47:36 +0200

    Adde implement code for max arg to be not a number

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,9 @@
 export default function randomNumber(min, max) {
     if(typeof min !== 'number')
         throw new Error('Min arg is not a number')
+    
+    if(typeof max !== 'number')
+        throw new Error('Max arg is not a number')
 
 
 
