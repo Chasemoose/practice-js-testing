a65c0ffd3b32f273c3f8489b2afb0e31f85f5fb8
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-19 22:43:08 +0200

    Added implement code fir non-number case

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('Num 1 is returned when draw number from 1 to 1', () => {
     expect(randomNumber(1, 1)).toBe(1)
 })
+
+it('If min arg number is not a number throw error', () => {
+    expect( () => randomNumber('z', 5)).toThrow('Min arg is not a number')
+})
