db392a1ea9da11d9e0190587cafb62eca5d58c91
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-19 23:05:24 +0200

    added testing and implementation for proper number ranges

--- 03/app.js
+++ 03/app.js
@@ -5,6 +5,9 @@ export default function randomNumber(min, max) {
     if(typeof max !== 'number' && isNaN(max))
         throw new Error('Max arg is not a number')
 
+    if(min > max) 
+        throw new Error('Min arg has to be lower than max arg')
+
 
 
     return Math.random() * (max - min) + min
--- 03/app.test.js
+++ 03/app.test.js
@@ -12,3 +12,7 @@ it('If min arg number is not a number throw error', () => {
 it('If max arg number is not a number throw error', () => {
     expect( () => randomNumber(5, 'z')).toThrow('Max arg is not a number')
 })
+
+it('If min number is lower than max number', () => {
+    expect( () => randomNumber(4, 3).toThrow('Min arg has to be lower than max arg'))
+})
