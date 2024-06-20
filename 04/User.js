e06e653ff7d202bb11dbdad6d46036d8878c197f
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-20 22:20:44 +0200

    Finished ex 4 in JS Testing practice part

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,32 @@
 export default class User {
+    constructor(userObj) {
+        const { email, password } = userObj
+
+        this.email = email
+        this.password = password
+
+        if(!email.includes('@')) {
+            throw new Error('This is wrong email')
+        }
+
+        if(password.length < 8) {
+            throw new Error('Password is no long enough')
+        }
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getPassword() {
+        return this.password
+    }
+
+    login() {
+        return this.email.includes('devmentor.pl')
+    }
+
+
+
 
 }
