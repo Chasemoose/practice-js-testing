d36671049ed72eaef13a59da2f980023fb9d69ae
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-20 22:50:48 +0200

    Made some partial things in ex5

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,11 @@
 import DB from './DB';
+
+describe('Insert method testing', ()=> {
+    it('Get 1 when first number added', () => {
+        const dataBase = new DB();
+        await dataBase.insert({id: 2, color: 'green'});
+
+        const rows = await dataBase.getRows()
+
+    })
+})
