12068b38a931351c64b9b9812c76a8874a511d04
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-21 18:23:00 +0200

    Finished 1 test and started 2nd

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,11 +1,24 @@
 import DB from './DB';
 
-describe('Insert method testing', ()=> {
-    it('Get 1 when first number added', () => {
+describe('Insert method testing', () => {
+    it('Get 1 when first number added', async () => {
         const dataBase = new DB();
         await dataBase.insert({id: 2, color: 'green'});
 
         const rows = await dataBase.getRows()
-
+        const rowsLength = rows.length
+        expect(rowsLength).toBe(1)
     })
+
+
+    it('ID can be only number! If no reject ', async () => {
+        const dataBase = new DB();
+        await dataBase.insert({id: 'a', color: 'green'});
+
+        const rows = await dataBase.getRows()
+        const rowsLength = rows.length
+        expect(rowsLength).toBe(1)
+    })
+
+
 })
