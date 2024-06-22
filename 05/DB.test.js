530130c23c69b3d152fa6ec241fcb930f19d35b6
Jarek Stelmach <j.stelmach@onet.eu>
2024-06-22 23:06:14 +0200

    Added some more tests to ex5 in testing part

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -13,12 +13,63 @@ describe('Insert method testing', () => {
 
     it('ID can be only number! If no reject ', async () => {
         const dataBase = new DB();
-        await dataBase.insert({id: 'a', color: 'green'});
 
-        const rows = await dataBase.getRows()
-        const rowsLength = rows.length
-        expect(rowsLength).toBe(1)
+        try {
+            await dataBase.insert({id: 'a', color: 'green'});
+
+        } catch(error) {
+            expect(error).toBe('ID can be only number!')
+
+        }
     })
+
+    it('ID can\'t be duplicated! If it is reject ', async () => {
+        const dataBase = new DB();
+
+        try {
+            await dataBase.insert({id: 1, color: 'green'});
+            await dataBase.insert({id: 1, color: 'yellow'});
+
+        } catch(error) {
+            expect(error).toBe('ID can\'t be duplicated!')
+
+        }
+    })
+
+
+})
+
+describe('Select method testing', () => {
+    it('Select existing item by ID', async () => {
+        const dataBase = new DB();
+        const data = { id: 1, color: 'blue' };
+        await dataBase.insert(data);
+
+        const selectedItem = await dataBase.select(1);
+        expect(selectedItem).toEqual(data);
+    });
+
+    it('Reject when ID not found', async () => {
+        const dataBase = new DB();
+
+        try {
+            await dataBase.select(998934);
+        } catch (error) {
+            expect(error).toBe('ID not found');
+        }
+    });
+
+    it('Select should reject if no ID provided', async () => {
+        const dataBase = new DB();
+
+        try {
+            await dataBase.select();
+        } catch (error) {
+            expect(error).toBe('ID not found');
+        }
+    });
+
+
 
 
 })
